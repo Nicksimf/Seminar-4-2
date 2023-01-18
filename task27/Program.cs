4ef6e9c789af63c13e69bc9da7e6f7aecd49ab70
Nick <scream.mmcc@mail.ru>
2023-01-18 21:35:29 +0300

    test 27

---
+++ task27/Program.cs
@@ -0,0 +1,7 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите ряд чисел, разделенных запятой : ");
+string? seriesOfNumbers = Console.ReadLine();
+
+seriesOfNumbers = seriesOfNumbers + ",";
+
